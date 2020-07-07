576305090da93fe3aaf62b5c6681d088e047c840
azarmadr <azarshaik454@gmail.com>
2020-07-07 16:22:09 +0530

    driver basic functions complete

--- tb/sdc/drv.h
+++ tb/sdc/drv.h
@@ -12,7 +12,7 @@ SC_MODULE(drv_sdc){
    sc_in<sc_bv<12> >   m_reg;
    sc_in<bool>         req_ack,  rd_v,   wr_nxt,  init_f;
 
-   sc_port<sc_fifo_out_if<pkt*> > sdc_ap;
+   //sc_port<sc_fifo_out_if<pkt*> > sdc_drv_f;
 
    void driver();
    void req_a();
@@ -33,18 +33,14 @@ void drv_sdc::driver(){
 
       while(!init_f) wait (1,SC_NS);
       req->write(true);
-
-      p->req_len = req_len->read();
-      p->wr_rd   = wr_rd->read();
-      p-addr     = addr->read();
-
       (p->wr_rd)?
 	 while(!rd_v)   wait (1,SC_NS):
 	 while(!wr_nxt) wait (1,SC_NS);
+
       while(wr_nxt || rd_v){
 	 (p->wr_rd)?
-	    p->data.push_back(rdata->read()):
-	    p->data.push_back(wdata->read());
+	    wdata->write(p->data.pop_back ()):
+	    rdata->read (p->data.push_back());
 	 wait();
       }
    }
